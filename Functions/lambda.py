6c92bb02556f59e2a9c8aa181575f1dfa3e1c46b
shijohimself <datawithshijo@gmail.com>
2026-04-22 00:38:27 +0200

    lambda functions

---
+++ Functions/lambda.py
@@ -0,0 +1,6 @@
+# lamda = anonymous functions
+# lambda function does not have any name
+
+add_nums = lambda n1 , n2 , n3: n1 + n2 + n3
+
+print(add_nums(10,20,30))
---
+++ Functions/lambda1.py
@@ -0,0 +1,5 @@
+make_list = lambda n : [i for i in range(n)]
+
+list1 = make_list(6)
+
+print(list1)
